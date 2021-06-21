ef2245dc058aa00de422893bd78e844b1ed02329
miftahhadi817 <ajago6028@gmail.com>
2021-06-21 18:21:57 +0100

    Membuat sebuah class dengan 8 (delapan) atribut, 4 (empat) method

---
+++ src/KaryawanTetap.java
@@ -0,0 +1,14 @@
+import java.sql.Date;
+
+public class KaryawanTetap {
+    private int id;
+    private int noKk;
+    private String namaLengkap;
+    private String jenisKelamin;
+    private Date tanggalLahir;
+    private String riwayatPendidikan;
+    private String jabatanSekarang;
+    private Date tanggalKontrak;
+
+    
+}
