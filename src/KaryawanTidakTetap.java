1d376f8e101a6ca619f256e9daaf1667f0267cf5
miftahhadi817 <ajago6028@gmail.com>
2021-06-22 08:42:07 +0100

    Pada class no. 2 tambahkan Getter and Setter untuk seluruh atribut

--- src/KaryawanTidakTetap.java
+++ src/KaryawanTidakTetap.java
@@ -11,6 +11,29 @@ public class KaryawanTidakTetap extends KaryawanTetap {
         System.out.println("Menampilkan Data Posisi Jabatan PegawaiTidakTetap");
     }
 
+    @Override
+    public String getNoKk() {
+        return noKk;
+    }
 
+    @Override
+    public void setNoKk(String noKk) {
+        this.noKk = noKk;
+    }
 
+    public String getPosisiJabatan() {
+        return posisiJabatan;
+    }
+
+    public void setPosisiJabatan(String posisiJabatan) {
+        this.posisiJabatan = posisiJabatan;
+    }
+
+    public String getLamaBekerja() {
+        return lamaBekerja;
+    }
+
+    public void setLamaBekerja(String lamaBekerja) {
+        this.lamaBekerja = lamaBekerja;
+    }
 }
