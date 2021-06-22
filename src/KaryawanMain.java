a97351eb5b95bc012ef0d710f3a43f17c7392ac4
miftahhadi817 <ajago6028@gmail.com>
2021-06-22 05:51:09 +0100

    Membuat sebuah class baru untuk menciptakan object dari soal no. 1 dan no. 2

---
+++ src/KaryawanMain.java
@@ -0,0 +1,6 @@
+public class KaryawanMain {
+    public static void main(String[] args) {
+        KaryawanTetap karyawanTetap = new KaryawanTetap();
+        KaryawanTidakTetap karyawanTidakTetap = new KaryawanTidakTetap();
+    }
+}
