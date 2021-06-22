69f79b1b60173e8235f1b9c9f39aeab20608116c
miftahhadi817 <ajago6028@gmail.com>
2021-06-22 08:16:01 +0100

    Pada class no. 3 tambahkan 2 (dua) object untuk tiap constructor class no. 1, sehingga total ada 6 (enam) object class no. 1

--- src/KaryawanMain.java
+++ src/KaryawanMain.java
@@ -1,6 +1,22 @@
 public class KaryawanMain {
     public static void main(String[] args) {
-        KaryawanTetap karyawanTetap = new KaryawanTetap();
-        KaryawanTidakTetap karyawanTidakTetap = new KaryawanTidakTetap();
+        KaryawanTetap karyawanTetap1 = new KaryawanTetap();
+        KaryawanTetap karyawanTetap2 = new KaryawanTetap();
+
+        KaryawanTetap karyawanTetap3 = new KaryawanTetap();
+        KaryawanTetap karyawanTetap4 = new KaryawanTetap();
+
+        KaryawanTetap karyawanTetap5 = new KaryawanTetap();
+        KaryawanTetap karyawanTetap6 = new KaryawanTetap();
+
+
+        KaryawanTetap miftah = new KaryawanTetap("732832","Miftah","Laki-laki");
+        KaryawanTetap hadi = new KaryawanTetap("832382","Hadi","Laki-laki");
+
+        KaryawanTetap amanah = new KaryawanTetap("678543", "Amanah","Perempuan");
+        KaryawanTetap putri = new KaryawanTetap("490873","Putri","Perempuan");
+
+        KaryawanTetap yanto = new KaryawanTetap("987451","Yanto","Laki-laki");
+        KaryawanTetap eli = new KaryawanTetap("201368", "Eli","Perempuan");
     }
 }
